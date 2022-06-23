2286350ff18d03de9a0fe91475e4e8847a886d71
Konstantin Borovik <kb@lab5.ca>
2022-06-23 07:23:44 -0400

    udpate terraform output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -14,3 +14,7 @@ output "google_region" {
 output "gke_name" {
   value = try(google_container_cluster.gke1.name, null)
 }
+
+output "gke_proxy_name" {
+  value = try(google_compute_instance.gke_proxy1.name, null)
+}
